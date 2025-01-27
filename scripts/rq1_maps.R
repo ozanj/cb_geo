2a25988c90680969190d33a1c42af1ceaa791363
cyouh95 <cyouh95@g.ucla.edu>
2025-01-26 20:58:54 -0800

    add draft of rq1 maps

---
+++ scripts/rq1_maps.R
@@ -0,0 +1,256 @@
+library(leaflet)
+library(tidyverse)
+library(formattable)
+library(htmlwidgets)
+
+
+# Load data
+
+source(file.path('scripts', 'directories.R'))
+source(file.path(scripts_dir, 'append_census.R'))
+source(file.path(scripts_dir, 'metro_eps_codes.R'))
+
+all_codes <- list(
+  philly = list(name = 'Philadelphia', eps = philly_eps_codes),
+  dallas = list(name = 'Dallas', eps = dallas_eps_codes),
+  atl = list(name = 'Atlanta', eps = atl_eps_codes),
+  chicago = list(name = 'Chicago', eps = chi_eps_codes),
+  cleveland = list(name = 'Cleveland', eps = cleveland_eps_codes),
+  northern_nj = list(name = 'Northern New Jersey', eps = nj_north_metro_eps_codes),
+  houston = list(name = 'Houston', eps = htown_eps_codes),
+  bay_area = list(name = 'Bay Area', eps = bay_eps_codes),
+  long_island = list(name = 'Long Island', eps = long_island_eps_codes),
+  ny_ny = list(name = 'New York, New York', eps = nyny_metro_eps_codes),
+  detroit = list(name = 'Detroit', eps = detroit_eps_codes),
+  boston = list(name = 'Boston', eps = boston_eps_codes),
+  miami = list(name = 'Miami', eps = miami_eps_codes),
+  dmv = list(name = 'DMV', eps = dmv_eps_codes),
+  orange_county = list(name = 'Orange County', eps = orange_county_eps_codes),
+  san_diego = list(name = 'San Diego', eps = san_diego_eps_codes),
+  los_angeles = list(name = 'Los Angeles', eps = los_angeles_eps_codes)
+)
+
+regions_data <- data.frame(
+  region = flatten_chr(map(names(all_codes), \(x) rep(x, length(all_codes[[x]]$eps)))),
+  region_name = flatten_chr(map(names(all_codes), \(x) rep(all_codes[[x]]$name, length(all_codes[[x]]$eps)))),
+  eps = flatten_chr(map(names(all_codes), \(x) all_codes[[x]]$eps))
+) %>% 
+  left_join(allyr_anal_tract_sf %>% filter(year == 2020) %>% select(eps, geometry), by = 'eps') %>% 
+  group_by(region, region_name) %>% 
+  summarise(
+    eps = list(unique(eps)),
+    geometry = st_union(geometry),
+    latitude = st_coordinates(st_centroid(geometry))[, 1],
+    longitude = st_coordinates(st_centroid(geometry))[, 2],
+    .groups = 'drop'
+  )
+
+base_vars <- list(
+  sum_tot_all = list(name = 'Total Population', abbrev = 'pop'),
+  med_inc_house = list(name = 'Median Household Income', abbrev = 'income'),
+  pct_nhisp_white = list(name = '% White, non-Hispanic', abbrev = 'nhisp_white'),
+  pct_nhisp_black = list(name = '% Black, non-Hispanic', abbrev = 'nhisp_black'),
+  pct_hisp_all = list(name = '% Hispanic', abbrev = 'hisp_all'),
+  pct_nhisp_asian = list(name = '% Asian, non-Hispanic', abbrev = 'nhisp_asian'),
+  pct_pov_yes = list(name = '% Poverty', abbrev = 'pov'),
+  pct_edu_baplus_all = list(name = '% Bachelors', abbrev = 'edu')
+)
+
+format_vars <- function(data_df) {
+  data_df %>% 
+    mutate(
+      sum_tot_all_text = format(round(sum_tot_all), big.mark = ',', trim = T),
+      med_inc_house_text = currency(med_inc_house, digits = 0L),
+      pct_nhisp_white_text = if_else(is.na(pct_nhisp_white), 'NA', paste0(sprintf('%.1f', pct_nhisp_white), '%')),
+      pct_nhisp_black_text = if_else(is.na(pct_nhisp_black), 'NA', paste0(sprintf('%.1f', pct_nhisp_black), '%')),
+      pct_hisp_all_text = if_else(is.na(pct_hisp_all), 'NA', paste0(sprintf('%.1f', pct_hisp_all), '%')),
+      pct_nhisp_asian_text = if_else(is.na(pct_nhisp_asian), 'NA', paste0(sprintf('%.1f', pct_nhisp_asian), '%')),
+      pct_pov_yes_text = if_else(is.na(pct_pov_yes), 'NA', paste0(sprintf('%.1f', pct_pov_yes), '%')),
+      pct_edu_baplus_all_text = if_else(is.na(pct_edu_baplus_all), 'NA', paste0(sprintf('%.1f', pct_edu_baplus_all), '%'))
+    )
+}
+
+eps_shapes_from_tract <- allyr_anal_tract_sf %>%
+  filter(eps %in% flatten_chr(regions_data$eps)) %>% 
+  select(year, eps, geometry) %>%
+  group_by(year, eps) %>% 
+  summarise(
+    geometry = st_union(geometry),
+    .groups = 'drop'
+  )
+
+eps_data <- allyr_anal_eps_sf %>% 
+  filter(eps %in% flatten_chr(regions_data$eps)) %>%
+  select(year, eps, eps_name, n_tracts, sum_tot_all, med_inc_house, pct_nhisp_white, pct_nhisp_black, pct_hisp_all, pct_nhisp_asian, pct_pov_yes, pct_edu_baplus_all, geometry) %>% 
+  rename('geometry_eps' = 'geometry') %>% 
+  as.data.frame() %>% 
+  left_join(as.data.frame(eps_shapes_from_tract), by = c('year', 'eps')) %>%
+  st_sf(sf_column_name = 'geometry') %>% 
+  format_vars()
+
+eps_data %>% as.data.frame() %>% 
+  group_by(year) %>% 
+  summarise(
+     across(sum_tot_all:pct_edu_baplus_all, ~sum(!is.na(.x)))
+  ) %>% View()  # Asian in 1980 data missing
+
+tract_data <- allyr_anal_tract_sf %>% 
+  filter(eps %in% flatten_chr(regions_data$eps)) %>% 
+  select(year, eps, eps_name, proportion, tot_all, med_inc_house, pct_nhisp_white, pct_nhisp_black, pct_hisp_all, pct_nhisp_asian, pct_pov_yes, pct_edu_baplus_all, geometry) %>% 
+  rename('sum_tot_all' = 'tot_all') %>% 
+  format_vars()
+
+tract_data %>% as.data.frame() %>% 
+  group_by(year) %>% 
+  summarise(
+    across(sum_tot_all:pct_edu_baplus_all, ~sum(!is.na(.x)))
+  ) %>% View()  # Asian in 1980 data missing
+
+
+# Map functions
+
+get_palette <- function(variable, domain, level, pal = 'YlGnBu') {
+  palette <- colorNumeric(pal, domain)
+  label_format <- labelFormat()
+  
+  if (str_detect(variable, '_inc_')) {
+    if (level == 'tract') {
+      bins <- pretty(range(domain, na.rm = T), n = 6)
+    } else {
+      bins <- c(0, 50, 75, 100, 125, 150, 175, 200, 225, 250) * 1000
+    }
+    palette <- colorBin(pal, domain, bins)
+    
+    label_format <- function(type, cuts) {
+      cuts <- cuts / 1000
+      
+      f <- cuts[[2]]
+      l <- cuts[[length(cuts) - 1]]
+      delta <- cuts[[2]] - cuts[[1]]
+      
+      cuts <- paste0('$', cuts, '-', cuts + delta - 1, 'k')
+      cuts[[1]] <- paste0('<$', f, 'k')
+      cuts[[length(cuts) - 1]] <- paste0('$', l, 'k+')
+      cuts[[length(cuts)]] <- 'NA'
+      
+      cuts
+    }
+  } else if (str_detect(variable, 'pct_')) {
+    bins <- pretty(range(0, 100), n = 5)
+    palette <- colorBin(pal, domain, bins)
+    
+    label_format <- function(type, cuts) {
+      f <- cuts[[2]]
+      l <- cuts[[length(cuts) - 1]]
+      delta <- cuts[[2]] - cuts[[1]]
+      
+      cuts <- paste0(cuts, '-', cuts + delta - 1, '%')
+      cuts[[length(cuts) - 1]] <- '80-100%'
+      cuts[[length(cuts)]] <- 'NA'
+      
+      cuts
+    }
+  }
+  
+  list(palette = palette, label_format = label_format)
+}
+
+create_rq1_map <- function(metros) {
+  js <- read_file(file.path(scripts_dir, 'rq1_maps.js'))
+  
+  choices <- list(region_choices = regions_data %>% filter(region %in% metros) %>% select(region, region_name, latitude, longitude))
+  
+  highlight_shp <- highlightOptions(weight = 2, color = '#606060', dashArray = '', bringToFront = T, sendToBack = T)
+  
+  yrs <- c(1980, 2000, 2020)
+  
+  m <- leaflet() %>%
+    addProviderTiles(providers$CartoDB.Positron) %>%
+    
+    addMiniMap(tiles = providers$CartoDB.Positron,
+               toggleDisplay = T) %>%
+    
+    addEasyButton(easyButton(
+      icon = 'fa-crosshairs', title = 'Toggle View',
+      onClick = JS("function(btn, map){ let zoom = $(btn.button).attr('data-national'); $('.custom-control').slideUp(); if (zoom === 'true') { let $sel = $('input[name=\"metro-choice\"]:checked'); map.setView([$sel.attr('data-lng'), $sel.attr('data-lat')], 8.2); $(btn.button).attr('data-national', false); $('#view-btn').html('National View'); } else { map.setView([39.828, -98.580], 4); $(btn.button).attr('data-national', true); $('#view-btn').html('MSA View'); }}"))) %>%
+    
+    addEasyButton(easyButton(
+      icon = 'fa-globe', title = 'Select Metro Area',
+      onClick = JS("function(btn, map){ $('.custom-control').not('#metro-control').slideUp(); $('#metro-control').slideToggle(); }")))
+    
+  for (metro in metros) {
+    print(metro)
+    
+    region <- regions_data %>% filter(region == metro)
+    eps <- eps_data %>% filter(eps %in% region$eps[[1]])
+    tract <- tract_data %>% filter(eps %in% region$eps[[1]])
+    
+    m <- m %>%
+      
+      # Metro outline
+      addPolylines(data = region$geometry, color = 'black', weight = 2, group = 'MSA', options = c(className = paste0('metro-shape metro-line metro-', metro)))
+    
+    for (y in yrs) {
+      m <- m %>%
+        
+        # EPS outline
+        addPolylines(data = eps %>% filter(year == y), opacity = 1, color = 'purple', weight = 2, options = c(className = paste0('metro-shape metro-', metro, ' level-tract year-', y)))
+    }
+    
+    for (v in names(base_vars)) {
+      color_pal_eps <- get_palette(v, eps[[v]], 'eps')
+      color_pal_tract <- get_palette(v, tract[[v]], 'tract')
+      
+      for (y in yrs) {
+        m <- m %>% 
+          
+          # Shapes
+          addPolygons(data = eps %>% filter(year == y), opacity = 1, color = '#808080', weight = 1, dashArray = '3', fillOpacity = 0.8, smoothFactor = 0.2, fillColor = ~color_pal_eps$palette(get(v)), label = ~paste0('<b>', eps, ' - ', eps_name, '</b><br>', get(paste0(v, '_text'))) %>% lapply(htmltools::HTML), group = paste0('MSA by ', base_vars[[v]]$name), highlightOptions = highlight_shp, options = pathOptions(className = paste0('metro-shape metro-', metro, ' level-eps year-', y))) %>% 
+          addPolygons(data = tract %>% filter(year == y), opacity = 1, color = '#808080', weight = 1, dashArray = '3', fillOpacity = 0.8, smoothFactor = 0.2, fillColor = ~color_pal_tract$palette(get(v)), label = ~paste0('<b>', eps, ' - ', eps_name, '</b><br>', get(paste0(v, '_text'))) %>% lapply(htmltools::HTML), group = paste0('MSA by ', base_vars[[v]]$name), highlightOptions = highlight_shp, options = pathOptions(className = paste0('metro-shape metro-', metro, ' level-tract year-', y)))
+      }
+      
+      metro_specific_vars <- c('sum_tot_all', 'med_inc_house')
+      class_name <- paste0('info legend legend-', base_vars[[v]]$abbrev)
+      
+      if (v %in% metro_specific_vars) {
+        class_name <- paste0(class_name, '-', metro)
+      }
+      
+      if (v %in% metro_specific_vars || metro == metros[[1]]) {
+        m <- m %>%
+          addLegend(data = eps,
+                    position = 'topright', pal = color_pal_eps$palette, values = ~get(v),
+                    title = base_vars[[v]]$name,
+                    className = paste0(class_name, '-eps'),
+                    labFormat = color_pal_eps$label_format,
+                    na.label = 'N/A',
+                    opacity = 1) %>%
+  
+          addLegend(data = tract,
+                    position = 'topright', pal = color_pal_tract$palette, values = ~get(v),
+                    title = base_vars[[v]]$name,
+                    className = paste0(class_name, '-tract'),
+                    labFormat = color_pal_tract$label_format,
+                    na.label = 'N/A',
+                    opacity = 1)
+      }
+    }
+  }
+    
+  m %>% 
+    addLayersControl(
+      position = c('bottomleft'),
+      baseGroups = c('MSA', str_c('MSA by ', flatten_chr(map(names(base_vars), \(x) base_vars[[x]]$name)))),
+      options = layersControlOptions(collapsed = F)
+    ) %>% 
+    htmlwidgets::onRender(js, choices)
+}
+
+
+# Run/save maps
+
+names(all_codes)
+
+create_rq1_map(c('bay_area'))
+
+saveWidget(create_rq1_map(c('bay_area', 'philly', 'chicago')), file.path('.', 'results', 'maps', 'rq1_map_bayarea_philly_chicago.html'), background = 'transparent', selfcontained = T)
---
+++ scripts/rq1_maps.js
@@ -0,0 +1,204 @@
+function(el, x, choices) {
+  
+  console.log(choices);
+  
+  let myMap = this;
+  
+  let region = choices.region_choices.region,
+      region_name = choices.region_choices.region_name,
+      latitude = choices.region_choices.latitude,
+      longitude = choices.region_choices.longitude;
+  
+  // buttons
+  
+  $('.easy-button-button').css('width', 'auto');
+  $('.easy-button-button .button-state .fa').css({'float': 'left', 'margin-top': '8px'});
+  
+  $('button[title="Toggle View"] .button-state').append('<span id="view-btn" style="display: inline-block; float: left; padding-left: 5px;">National View</span>');
+  $('button[title="Select Metro Area"] .button-state').append('<span style="display: inline-block; float: left; padding-left: 5px;">Select Metro Area</span>');
+  
+  // metro selection options
+  
+  let metroControlHTML = '<div id="metro-control" class="leaflet-control-layers leaflet-control leaflet-control-layers-expanded custom-control" style="width: auto; height: auto;">';
+  
+  region_name.forEach(function(curr, idx) {
+    metroControlHTML += '<div><input type="radio" class="leaflet-control-layers-selector" name="metro-choice" data-region="' + region[idx] + '" data-lat="' + latitude[idx] + '" data-lng="' + longitude[idx] + '"><span> ' + curr + '</span></div>';
+  });
+    
+  metroControlHTML += '</div>';
+  
+  $('button[title="Select Metro Area"]').parent().after(metroControlHTML);
+  
+  // year selection options
+  
+  let yearControlHTML = '<p style="margin: 5px; font-weight: 600;">Year</p><div style="display: flex;">';
+  
+  ['1980', '2000', '2020'].forEach(function(curr, idx) {
+    yearControlHTML += '<div' + (idx === 0 ? '' : ' style="margin-left: 5px;"') + '><input type="radio" class="leaflet-control-layers-selector" name="year-choice" data-year="' + curr + '"><span>' + curr + '</span></div>';
+  });
+  
+  yearControlHTML += '</div>';
+  
+  $('.leaflet-control-layers-base').append(yearControlHTML);
+  
+  // level selection options
+  
+  let levelControlHTML = '<p style="margin: 5px; font-weight: 600;">Level</p><div style="display: flex;">';
+  
+  ['EPS', 'Tract'].forEach(function(curr, idx) {
+    levelControlHTML += '<div' + (idx === 0 ? '' : ' style="margin-left: 5px;"') + '><input type="radio" class="leaflet-control-layers-selector" name="level-choice" data-level="' + curr.toLowerCase() + '"><span>' + curr + '</span></div>';
+  })
+  
+  levelControlHTML += '</div>';
+  
+  $('.leaflet-control-layers-base').append(levelControlHTML);
+    
+  // selection text
+  
+  selTextHTML = '<div id="selection-text" style="padding: 10px; display: inline-block; font-weight: 900; color: #444;"></div>';
+  
+  $('.leaflet > .leaflet-control-container > .leaflet-top.leaflet-left').append(selTextHTML);
+  
+  // handle selections
+    
+  $('input[name="metro-choice"]').on('change', function(e) {
+    let $this = $(this);
+    
+    let metro = $this.attr('data-region'),
+        lat = $this.attr('data-lat'),
+        lng = $this.attr('data-lng');
+        
+    active_attr.active_metro = metro;
+    update_base_layer();
+    
+    myMap.setView([lng, lat], 8.2);
+    
+    $('button[title="Toggle View"]').attr('data-national', false);
+    $('#view-btn').html('National View');
+    
+    update_sel_text();
+    update_legend();
+  });
+  
+  $('input[name="year-choice"]').on('change', function(e) {
+    let $this = $(this);
+    
+    let year = $this.attr('data-year');
+    
+    active_attr.active_year = year;
+    update_base_layer();
+    
+    update_sel_text();
+  });
+  
+  $('input[name="level-choice"]').on('change', function(e) {
+    let $this = $(this);
+    
+    let level = $this.attr('data-level');
+    
+    active_attr.active_level = level;
+    update_base_layer();
+    
+    update_legend();
+  });
+  
+  let update_base_layer = function() {
+    $('.metro-shape').css('display', 'none');
+    
+    if (active_attr.active_base === 'MSA') {
+      $('.metro-' + active_attr.active_metro + '.metro-line').css('display', 'inherit');
+    } else {
+      $('.metro-' + active_attr.active_metro + '.level-' + active_attr.active_level + '.year-' + active_attr.active_year).css('display', 'inherit');
+    }
+  };
+  
+  // disable options
+  
+  $('input').on('change', function(e) {
+    $('input').prop('disabled', false);
+    
+    if (active_attr.active_base === 'MSA') {
+      if (!$('input[data-year]:checked').length) {
+        $('input[data-year="1980"]').trigger('click');
+      }
+      if (!$('input[data-level]:checked').length) {
+        $('input[data-level="eps"]').trigger('click');
+      }
+      
+      $('input[data-year], input[data-level]').prop('disabled', true);
+    }
+    
+    if (active_attr.active_base === 'MSA by % Asian, non-Hispanic') {
+      $('input[data-year="1980"]').prop('disabled', true);
+      if (active_attr.active_year === '1980') {
+        $('input[data-year="2000"]').trigger('click');
+      }
+    }
+  })
+  
+  // handle selection text update
+  
+  let update_sel_text = function() {
+    let sel_metro = $('input[data-region="' + active_attr.active_metro + '"]').next().text(),
+      sel_year = active_attr.active_year;
+    $('#selection-text').text(sel_metro + ' in ' + sel_year);
+  };
+  
+  // handle legend update
+  
+  let update_legend = function(sel_name) {
+    $('.legend').css('display', 'none');
+    switch (active_attr.active_base) {
+      case 'MSA by Total Population':
+        $('.legend-pop-' + active_attr.active_metro + '-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by Median Household Income':
+        $('.legend-income-' + active_attr.active_metro + '-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by % White, non-Hispanic':
+        $('.legend-nhisp_white-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by % Black, non-Hispanic':
+        $('.legend-nhisp_black-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by % Hispanic':
+        $('.legend-hisp_all-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by % Asian, non-Hispanic':
+        $('.legend-nhisp_asian-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by % Poverty':
+        $('.legend-pov-' + active_attr.active_level).css('display', 'inherit');
+        break;
+      case 'MSA by % Bachelors':
+        $('.legend-edu-' + active_attr.active_level).css('display', 'inherit');
+        break;
+    }
+  };
+  
+  // handle base layer selection
+
+  myMap.on('baselayerchange', function(e) {
+    active_attr.active_base = e.name;
+    update_legend();
+    
+    e.layer.bringToBack();
+    update_base_layer();
+  });
+  
+  // default settings on load
+      
+  let active_attr = {
+    active_base: 'MSA',
+    active_metro: region[0],
+    active_level: 'eps',
+    active_year: '1980'
+  };
+  
+  $('.legend, #metro-control').css('display', 'none');
+  
+  $('input[data-region="' + active_attr.active_metro + '"]').trigger('click');
+  $('input[data-year="' + active_attr.active_year + '"]').trigger('click');
+  $('input[data-level="' + active_attr.active_level + '"]').trigger('click');
+  
+}
